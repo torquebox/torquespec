73399a1ccebf1661d68fa3d2eb40fe57dc30d7a0
Jim Crossley <jim@crossleys.org>
2011-06-09 16:32:18 -0400

    Make compatible with 1.9, in which Strings are no longer Enumerable

--- lib/torquespec/daemon.rb
+++ lib/torquespec/daemon.rb
@@ -8,7 +8,7 @@ module TorqueSpec
     def initialize(opts={})
       puts "JC: create daemon opts=#{opts.inspect}"
       dir = opts['pwd'].to_s
-      raise "The 'pwd' option must contain a valid directory name" unless dir.any? && File.exist?(dir)
+      raise "The 'pwd' option must contain a valid directory name" if dir.empty? || !File.exist?(dir)
       Dir.chdir( dir ) do
         RSpec::Core::Runner.disable_autorun! # avoid a bunch of at_exit finalizer errors
         @options = RSpec::Core::ConfigurationOptions.new( opts['argv'].to_a )
